1b8a6cbbd112003a54ed1739c509272fe9d795c0
Naumov Andrei <naumov_a_a@outlook.com>
2021-12-18 17:37:18 +0300

    connect with backend

--- ui/src/routes/UserAds.js
+++ ui/src/routes/UserAds.js
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AdRecordsTable from "../components/AdRecordsTable";
+import AuthService from '../services/auth.service';
+import { Redirect } from "react-router-dom";
+import axios from 'axios';
+import authHeader from "../services/auth-header";
+
+const API_URL = "http://localhost:8080/ad/user/";
+
+const AdRecordsTableWrapper = props => {
+    const [ads, setAds] = useState([]);
+    const { _, userId, } = AuthService.getUser();
+
+    useEffect(() => {
+        axios
+            .get(API_URL + userId, { headers: authHeader() })
+            .then(res => {
+                //console.log(res);
+                setAds(res.data);
+            })
+    }, [userId, setAds]);
+
+    return <AdRecordsTable ads={ads} />;
+}
 
 const UserAds = props => {
-    return <AdRecordsTable />;
+    if (!AuthService.getUser()) {
+        return <Redirect to="/login" />
+    }
+
+    return <AdRecordsTableWrapper />
 };
 
 export default UserAds;
