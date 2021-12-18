ca50911ee5877dec6620b624fbd174f722ff76c4
DaniilKornilov <daniil-kornilov@yandex.ru>
2021-12-19 02:33:42 +0300

    home page layout

--- ui/src/routes/Home.js
+++ ui/src/routes/Home.js
@@ -1,10 +1,45 @@
 import React from "react";
 import {FilterModal} from "../components/FilterModal";
+import AdRecordsTable from "../components/AdRecordsTable";
+import Box from "@mui/material/Box";
+import {TextField} from "@mui/material";
+import Button from "@material-ui/core/Button";
 
 const Home = props => {
+    const [searchValue, setSearchValue] = React.useState("");
+
+    const handleClickSearch = () => {
+        console.log("search");
+    }
+
     return (
         <div>
-            <FilterModal />
+            <Box sx={{display: "flex"}}>
+                <Box sx={{width: 1 / 3, flex: 1}}>
+                    <FilterModal/>
+                </Box>
+                <Box sx={{width: 1 / 3, flex: 1, marginTop: 3}}>
+                    <TextField
+                        size="small"
+                        label="Поиск"
+                        variant="outlined"
+                        onChange={e => {
+                            setSearchValue(e.target.value);
+                        }}
+                        value={searchValue}
+                        fullWidth
+                    />
+                </Box>
+                <Box sx={{width: 1 / 3, flex: 1, marginTop: 3}}>
+                    <Button
+                        variant="outlined"
+                        onClick={handleClickSearch}
+                    >
+                        Поиск
+                    </Button>
+                </Box>
+            </Box>
+            <AdRecordsTable/>
         </div>
     );
 };
