0b361ff12b71b115d30f9429a76b5eabaa3716d6
Valeriy <chugajnov.va@edu.spbstu.ru>
2021-12-20 14:50:03 +0300

    Фикс previewImage

--- service/src/main/java/com/spbstu/edu/advertisement/mapper/AdMapper.java
+++ service/src/main/java/com/spbstu/edu/advertisement/mapper/AdMapper.java
@@ -10,15 +10,14 @@ import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
 import static com.spbstu.edu.advertisement.service.impl.ImageServiceImpl.DEFAULT_IMAGE_PATH;
-import static com.spbstu.edu.advertisement.service.impl.ImageServiceImpl.UPLOAD_PATH;
 
 @Mapper(componentModel = "spring", uses = {MetroMapper.class, SubCategoryMapper.class, UserMapper.class, AdService.class})
 public interface AdMapper {
     
     Ad toAd(AdDto adDto);
     
-    @Mapping(expression = "java(\"" + UPLOAD_PATH + "/\" + (ad.getImages() == null || ad.getImages().isEmpty() ? \""
-            + DEFAULT_IMAGE_PATH + "\" : ad.getImages().get(0).getPath()))", target = "previewImagePath")
+    @Mapping(expression = "java(ad.getImages() == null || ad.getImages().isEmpty() ? \""
+            + DEFAULT_IMAGE_PATH + "\" : ad.getImages().get(0).getPath())", target = "previewImagePath")
     AdDto toAdDto(Ad ad);
     
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
