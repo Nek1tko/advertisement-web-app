451e93c6fb53ec57b303ea1a2aee31fb49686240
DaniilKornilov <daniil-kornilov@yandex.ru>
2022-03-09 18:34:57 +0300

    fix according review

--- ui/src/components/tests/LocalizedDropzoneArea.test.js
+++ ui/src/components/tests/LocalizedDropzoneArea.test.js
@@ -1,7 +1,6 @@
 import '../LocalizedDropzoneArea'
 import LocalizedDropzoneArea from "../LocalizedDropzoneArea";
-import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
-import {configure, mount, shallow} from 'enzyme';
+import {mount, shallow} from 'enzyme';
 import {DropzoneArea} from "material-ui-dropzone";
 
 const props = {
@@ -11,54 +10,43 @@ const props = {
     onChange: jest.fn()
 };
 
-configure({adapter: new Adapter()});
-
-
 describe('LocalizedDropZoneAreaTests', () => {
+    const wrapper = shallow(<LocalizedDropzoneArea{...props} />);
+    const dropzoneAreaWrapper = wrapper.find(DropzoneArea);
+
     test('LocalizedDropZoneAreaRenderTest', () => {
         mount(<LocalizedDropzoneArea{...props} />);
     });
 
     test('LocalizedDropZoneAreaSnapshotTest', () => {
-        const wrapper = shallow(<LocalizedDropzoneArea{...props} />);
         expect(wrapper).toMatchSnapshot();
     });
 
     test('LocalizedDropZoneAreaOnChangeTest', () => {
-        const wrapper = shallow(<LocalizedDropzoneArea{...props} />);
-        const dropzoneAreaWrapper = wrapper.find(DropzoneArea);
         const onChange = 'onChange';
         dropzoneAreaWrapper.prop(onChange)();
         expect(props.onChange).toHaveBeenCalled();
     });
 
     test('LocalizedDropZoneAreaGetFileAddedMessageTest', () => {
-        const wrapper = shallow(<LocalizedDropzoneArea{...props} />);
-        const dropzoneAreaWrapper = wrapper.find(DropzoneArea);
         const fileName = 'file.jpg';
         const message = "Файл " + fileName + " успешно загружен";
         expect(dropzoneAreaWrapper.prop('getFileAddedMessage')(fileName)).toBe(message);
     });
 
     test('LocalizedDropZoneAreaGetFileLimitExceedMessageTest', () => {
-        const wrapper = shallow(<LocalizedDropzoneArea{...props} />);
-        const dropzoneAreaWrapper = wrapper.find(DropzoneArea);
         const filesLimit = 3;
         const message = "Достигнуто максимальное количество файлов: " + filesLimit;
         expect(dropzoneAreaWrapper.prop('getFileLimitExceedMessage')(filesLimit)).toBe(message);
     });
 
     test('LocalizedDropZoneAreaGetFileRemovedMessageTest', () => {
-        const wrapper = shallow(<LocalizedDropzoneArea{...props} />);
-        const dropzoneAreaWrapper = wrapper.find(DropzoneArea);
         const fileName = 'file.jpg';
         const message = "Файл " + fileName + " удален";
         expect(dropzoneAreaWrapper.prop('getFileRemovedMessage')(fileName)).toBe(message);
     });
 
     test('LocalizedDropZoneAreaGetDropRejectMessageTest', () => {
-        const wrapper = shallow(<LocalizedDropzoneArea{...props} />);
-        const dropzoneAreaWrapper = wrapper.find(DropzoneArea);
         const rejectedFile = new File(["foo"], 'file.jpg');
         const acceptedFile = 'ignored.jpg';
         const maxFileSize = 3;
