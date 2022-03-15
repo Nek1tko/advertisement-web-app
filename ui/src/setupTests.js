9e4c0b73190425c82547f8003950571c4125b898
DaniilKornilov <daniil-kornilov@yandex.ru>
2022-03-15 23:44:56 +0300

    add FilterModal.test.js

--- ui/src/setupTests.js
+++ ui/src/setupTests.js
@@ -2,6 +2,7 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+import 'regenerator-runtime/runtime'
 import '@testing-library/jest-dom';
 import {configure} from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
