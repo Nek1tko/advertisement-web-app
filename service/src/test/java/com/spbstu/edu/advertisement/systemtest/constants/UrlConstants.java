b20af3390f3b0ea63f3a08f97af1c014de7847e3
DaniilKornilov <daniil-kornilov@yandex.ru>
2022-04-05 22:46:11 +0300

    change urls

--- service/src/test/java/com/spbstu/edu/advertisement/systemtest/constants/UrlConstants.java
+++ service/src/test/java/com/spbstu/edu/advertisement/systemtest/constants/UrlConstants.java
@@ -1,9 +1,9 @@
 package com.spbstu.edu.advertisement.systemtest.constants;
 
 public class UrlConstants {
-    public static final String DATABASE_URL = "system-tests_db_1:4000";
+    public static final String DATABASE_URL = "localhost:4000";
 
-    public static final String MAIN_URL = "http://system-tests_frontend_1:3000/";
+    public static final String MAIN_URL = "http://localhost:3000/";
 
     public static final String SIGN_UP_URL = MAIN_URL + "sign-up";
 
